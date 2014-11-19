674243ca10bde00c3db3f0504751d7e7a5ba2c36
VasiliyShaitan <VasiliyShaitan@gmail.com>
2014-11-19 14:55:39 +0300

    Deleted all the files

--- server/Main.java
+++
@@ -1,9 +0,0 @@
-package com.ays.javachat.server;
-
-import com.ays.javachat.server.manager.ServerManager;
-
-public class Main {
-    public static void main(String[] args) {
-        new ServerManager();
-    }
-}
